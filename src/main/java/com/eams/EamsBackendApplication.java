7510aef1d4067aa537df2e72c619d902f5960964
feb-dylan <painghtoo9789@gmail.com>
2026-08-02 18:20:15 +0630

    Initial project of Eams backend

---
+++ src/main/java/com/eams/EamsBackendApplication.java
@@ -0,0 +1,12 @@
+package com.eams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EamsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EamsBackendApplication.class, args);
+	}
+}
